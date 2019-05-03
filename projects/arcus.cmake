2c8a4d1148f3feabb13e7121f1a6da29ca679e46
Lipu Fei <lipu.fei815@gmail.com>
2019-05-03 15:00:17 +0200

    Cleanup and fix arcus and savitar building

--- projects/arcus.cmake
+++ projects/arcus.cmake
@@ -1,6 +1,6 @@
 set(extra_cmake_args "")
 if(BUILD_OS_WINDOWS)
-    set(extra_cmake_args -DPYTHON_LIBRARY=${CMAKE_INSTALL_PREFIX}/libs/python35.lib -DPYTHON_INCLUDE_DIR=${CMAKE_INSTALL_PREFIX}/include -DPYTHON_SITE_PACKAGES_DIR=lib/site-packages -DMSVC_STATIC_RUNTIME=ON)
+    set(extra_cmake_args -DMSVC_STATIC_RUNTIME=ON)
 elseif(BUILD_OS_OSX)
     if(CMAKE_OSX_DEPLOYMENT_TARGET)
         list(APPEND extra_cmake_args
@@ -17,7 +17,12 @@ endif()
 ExternalProject_Add(Arcus
     GIT_REPOSITORY https://github.com/ultimaker/libArcus.git
     GIT_TAG origin/${CURA_ARCUS_BRANCH_OR_TAG}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_STATIC=ON -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX} ${extra_cmake_args}
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+               -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
+               -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+               -DCMAKE_INSTALL_LIBDIR=lib
+               -DBUILD_STATIC=ON -DBUILD_EXAMPLES=OFF
+               ${extra_cmake_args}
 )
 
 SetProjectDependencies(TARGET Arcus DEPENDS Sip Protobuf)
@@ -27,12 +32,14 @@ if(BUILD_OS_WINDOWS)
         GIT_REPOSITORY https://github.com/ultimaker/libArcus.git
         GIT_TAG origin/${CURA_ARCUS_BRANCH_OR_TAG}
         CMAKE_GENERATOR "MinGW Makefiles"
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_LIBDIR=lib-mingw -DBUILD_STATIC=ON -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DPROTOBUF_LIBRARY=${CMAKE_INSTALL_PREFIX}/lib-mingw/libprotobuf.a -DBUILD_PYTHON=OFF
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                   -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                   -DCMAKE_INSTALL_LIBDIR=lib-mingw
+                   -DBUILD_STATIC=ON -DBUILD_EXAMPLES=OFF -DBUILD_PYTHON=OFF
         BUILD_COMMAND mingw32-make
         INSTALL_COMMAND mingw32-make install
     )
 
     SetProjectDependencies(TARGET Arcus-MinGW DEPENDS Sip Protobuf-MinGW Arcus)
 endif()
-
-
--- projects/savitar.cmake
+++ projects/savitar.cmake
@@ -1,13 +1,4 @@
-if(BUILD_OS_LINUX)
-    set(savitar_cmake_command PATH=${CMAKE_INSTALL_PREFIX}/bin/:$ENV{PATH} LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib/ PYTHONPATH=${CMAKE_INSTALL_PREFIX}/lib/python3/dist-packages/:${CMAKE_INSTALL_PREFIX}/lib/python3.5:${CMAKE_INSTALL_PREFIX}/lib/python3.5/site-packages/ ${CMAKE_COMMAND})
-else()
-    set(savitar_cmake_command ${CMAKE_COMMAND})
-endif()
-
-set(extra_cmake_args "")
-if(BUILD_OS_WINDOWS)
-    set(extra_cmake_args -DPYTHON_LIBRARY=${CMAKE_INSTALL_PREFIX}/libs/python35.lib -DPYTHON_INCLUDE_DIR=${CMAKE_INSTALL_PREFIX}/include -DPYTHON_SITE_PACKAGES_DIR=lib/site-packages)
-elseif(BUILD_OS_OSX)
+if(BUILD_OS_OSX)
     if(CMAKE_OSX_DEPLOYMENT_TARGET)
         list(APPEND extra_cmake_args
             -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
@@ -23,13 +14,11 @@ endif()
 ExternalProject_Add(Savitar
     GIT_REPOSITORY https://github.com/ultimaker/libSavitar.git
     GIT_TAG origin/${CURA_SAVITAR_BRANCH_OR_TAG}
-    CMAKE_COMMAND ${savitar_cmake_command}
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
                -DCMAKE_INSTALL_LIBDIR=lib
                -DBUILD_STATIC=ON
-               ${extra_cmake_args}
 )
 
 SetProjectDependencies(TARGET Savitar DEPENDS Sip)
